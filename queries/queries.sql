e1b0017fe5e9530c23b5f8948ced3d801021cce2
Adrià Quiñoa Jimenez <adriaquinoajimenez@gmail.com>
2026-02-04 12:53:35 +0100

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -92,10 +92,10 @@ SELECT p.nombre, p.precio, f.nombre AS 'fabricante' FROM producto p INNER JOIN f
 SELECT p.nombre, p.precio, f.nombre AS 'fabricante' FROM producto p INNER JOIN fabricante f ON f.codigo = p.codigo_fabricante WHERE f.nombre LIKE '%w%';
 
 -- 32. Retorna un llistat amb el nom del producte, el seu preu i el nom del fabricant (fabricante), per a tots els productes amb un preu igual o superior a 180 €. Ordena els resultats, primer pel preu en ordre descendent i després pel nom del producte en ordre ascendent.
-
+SELECT p.nombre, p.precio, f.nombre AS 'fabricante' FROM producto p INNER JOIN fabricante f ON f.codigo = p.codigo_fabricante WHERE p.precio >= 180 ORDER BY p.precio DESC, p.nombre ASC;
 
 -- 33. Retorna un llistat amb el codi i el nom de fabricant (fabricante), solament d'aquells fabricants que tenen productes associats en la base de dades.
-
+SELECT DISTINCT f.codigo, f.nombre AS 'fabricante' FROM fabricante f INNER JOIN producto p ON p.codigo_fabricante = f.codigo;
 
 -- 34. Retorna un llistat de tots els fabricants que existeixen en la base de dades, juntament amb els productes que té cadascun d'ells. Inclou també els fabricants que no tenen cap producte. Mostra el nom del fabricant (fabricante) i el nom del producte (producto).
 
